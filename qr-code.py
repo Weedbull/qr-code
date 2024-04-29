d0f760898f8ac3458dd34dce424dc55aea88034c
Alex <abukovsky3131@mail.ru>
2024-04-29 21:58:33 +0300

    v1.0

--- qr-code.py
+++ qr-code.py
@@ -3,16 +3,16 @@ from segno import helpers
 def wifi():
     print("WIFI QR_CODE")
     qr_code = segno.helpers.make_wifi(input("ssid:"), input("WIFI Password:"), security="WPA")
-    qr_code.show()
+    qr_code.show(scale=20)
 def qr_code_url():
     print("URL QR_CODE")
     qr_code = segno.make_qr(input("URL for qrcode:"))
-    qr_code.show()
+    qr_code.show(scale=20)
     return
 def email():
     print("EMAIL QR_CODE")
     qr_code = segno.helpers.make_email("EMAIL for sends letter")
-    qr_code.show()
+    qr_code.show(scale=20)
     return
 
 print("That do you want QR CODE setup ?:")
